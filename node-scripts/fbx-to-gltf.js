ed9e9e56a852d1b8a47df01dbe48de37d08a6250
martin-banks <anotherbanksy@gmail.com>
2019-06-07 17:53:36 +1000

    changes target to mannequin model

--- node-scripts/fbx-to-gltf.js
+++ node-scripts/fbx-to-gltf.js
@@ -1,14 +1,19 @@
 const convert = require('fbx2gltf')
 const path = require('path')
 
+// After conversion run through a converter to change to gltf format
+// https://blackthread.io/gltf-converter/
+
 console.log(__dirname)
 
-const fileName = 'man_BaseMesh_Anim'
+// const fileName = 'man_BaseMesh_Anim'
+const fileName = 'mannequin/free3DmodelFBX'
+
 const input = path.join(__dirname, `../src/assets/${fileName}.fbx`)
 const output = path.join(__dirname, `../src/assets/${fileName}.glb`)
 
 convert(input, output, ['--khr-materials-unlit'])
-  .then(destPath => {
+.then(destPath => {
     console.log('conversion done')
     console.log(destPath)
   })
