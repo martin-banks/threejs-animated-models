00a1300e1b44a12edff3c7c881962136955a4960
martin-banks <anotherbanksy@gmail.com>
2019-06-07 12:51:59 +1000

    basic setup with sass and blank component

---
+++ src/components/_blank.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Blank component</h1>
+</template>
+
+<script>
+export default {
+  name: 'component-name',
+  props: {},
+  components: {},
+  data () {
+    return {
+    }
+  },
+  methods: {},
+  computed: {},
+
+  mounted () {},
+}
+</script>
+
+<style scoped lang="sass">
+
+
+</style>
