aa8091c96878d3ed4995bd20ef44e304b193b68b
martin-banks <anotherbanksy@gmail.com>
2019-06-07 13:15:18 +1000

    adds fbx to gltb converter

---
+++ node-scripts/fbx-to-gltf.js
@@ -0,0 +1,17 @@
+const convert = require('fbx2gltf')
+const path = require('path')
+
+console.log(__dirname)
+
+const fileName = 'man_BaseMesh_Anim'
+const input = path.join(__dirname, `../src/assets/${fileName}.fbx`)
+const output = path.join(__dirname, `../src/assets/${fileName}.glb`)
+
+convert(input, output, ['--khr-materials-unlit'])
+  .then(destPath => {
+    console.log('done')
+    console.log(destPath)
+  })
+  .catch(err => {
+    console.log('---ERROR---\n', err)
+  })
